692397859d1a42ba01975de1c9abb59e36262198
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-08-04 08:58:48 +0530

    Added: UC3

--- src/com/bl/EmpWage/EmployeeWage.java
+++ src/com/bl/EmpWage/EmployeeWage.java
@@ -20,4 +20,16 @@ public class EmployeeWage extends Main {
 		System.out.println("Employee Wage: " + emp_wage);
 	}
 
+	public void UC3() {
+
+		double emp_check = Math.floor(Math.random() * 10) % 3;
+		if (emp_check == IS_PARTTIME)
+			emp_hrs = 4;
+		if (emp_check == IS_FULLTIME)
+			emp_hrs = 8;
+		else
+			emp_hrs = 0;
+		emp_wage = emp_hrs * RATE_PER_HOUR;
+		System.out.println("Employee Wage: " + emp_wage);
+	}
 }
--- src/com/bl/EmpWage/Main.java
+++ src/com/bl/EmpWage/Main.java
@@ -1,15 +1,19 @@
 package com.bl.EmpWage;
 
 public class Main {
-	static int IS_FULLTIME = 1;
+	static int IS_FULLTIME = 2;
 	static int RATE_PER_HOUR = 20;
 	static int emp_hrs = 0;
 	static int emp_wage = 0;
+	static int FULL_DAY_HOURS = 8;
+	static int IS_PARTTIME = 1;
+	
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Calculation Program");
 		EmployeeWage obj = new EmployeeWage();
 		obj.EmpCheck();
 		obj.CalculateEmpWage();
+		obj.UC3();
 	}
 }
