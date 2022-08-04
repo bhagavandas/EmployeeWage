052c7af13741bcaa532bc022d5f9b21e8c9df798
bhagavandas <98629370+bhagavandas@users.noreply.github.com>
2022-08-04 08:56:37 +0530

    Added: UC2

--- src/com/bl/EmpWage/EmployeeWage.java
+++ src/com/bl/EmpWage/EmployeeWage.java
@@ -3,10 +3,21 @@ package com.bl.EmpWage;
 public class EmployeeWage extends Main {
 	public void EmpCheck() {
 		double emp_check = Math.floor(Math.random() * 10) % 2;
-		if(emp_check == IS_FULLTIME) 
+		if (emp_check == IS_FULLTIME)
 			System.out.println("Employee is Present");
-			else
-				System.out.println("Employee is Absent");	
+		else
+			System.out.println("Employee is Absent");
+	}
+
+	public void CalculateEmpWage() {
+
+		double emp_check = Math.floor(Math.random() * 10) % 2;
+		if (emp_check == IS_FULLTIME)
+			emp_hrs = 8;
+		else
+			emp_hrs = 0;
+		emp_wage = emp_hrs * RATE_PER_HOUR;
+		System.out.println("Employee Wage: " + emp_wage);
 	}
 
 }
--- src/com/bl/EmpWage/Main.java
+++ src/com/bl/EmpWage/Main.java
@@ -2,12 +2,14 @@ package com.bl.EmpWage;
 
 public class Main {
 	static int IS_FULLTIME = 1;
-	
-	
+	static int RATE_PER_HOUR = 20;
+	static int emp_hrs = 0;
+	static int emp_wage = 0;
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Calculation Program");
 		EmployeeWage obj = new EmployeeWage();
 		obj.EmpCheck();
+		obj.CalculateEmpWage();
 	}
 }
